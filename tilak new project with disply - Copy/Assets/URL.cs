caf0f8b551c79b4112908ed1b6f27336538dae71
rasaili tilak <rasailytilak101@gmail.com>
2022-02-15 15:17:50 +0900

    Initial commit

---
+++ tilak new project with disply - Copy/Assets/URL.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URL : MonoBehaviour
+{
+   
+    public void google()
+    {
+
+        Application.OpenURL("https://www.google.com/");
+            }
+    public void youtube()
+    {
+
+        Application.OpenURL("https://www.youtube.com/");
+            }
+}
---
+++ tilak new project with disply - Copy/Assets/script/link  start.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class linkstart : MonoBehaviour
+{
+    public void game()
+    {
+        SceneManager.LoadScene(1) ;
+    }
+
+    public void gme()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
